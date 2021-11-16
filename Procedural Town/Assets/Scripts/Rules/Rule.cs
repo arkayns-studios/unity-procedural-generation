c905cbba3284d67a2b80b69cb07a64b47aea2e12
Serge Maia <sergemaia9@gmail.com>
2021-11-16 17:52:07 +0000

    ADD: Scriptable rule for the L-System

---
+++ Procedural Town/Assets/Scripts/Rules/Rule.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Arkayns.P.PS {
+
+    [CreateAssetMenu(menuName = "Procedural City/Rule")]
+    public class Rule : ScriptableObject {
+
+        public string letter;
+        [SerializeField]
+        private string [] results = null;
+
+        public string GetResults () {
+            return results [0];
+        } // GetResults
+
+    } // Class Rule
+
+} // PS
